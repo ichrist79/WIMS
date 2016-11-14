f1a95d3eccdcc340a7450f3edeb3c3fcbbfef010
hmeriman <hmeriman@gmail.com>
2016-11-14 23:11:24 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,15 @@
+﻿<?php 
+
+	$servername = "localhost";
+	$username = "id155970_adix";
+	$password = "adix2016";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=id155970_wims", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
