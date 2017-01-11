7fbfd7b1af156cb40a5110351dc2257083e30f1e
Ioannis Christoudis <ioannis.christoudis@gmail.com>
2017-01-11 21:09:16 +0200

    responsive

--- active.php
+++ active.php
@@ -4,7 +4,7 @@
 
 session_start();
 include 'functions.php';
-$db = new Database("localhost", "root", "", "wimsfinal");
+$db = new Database("localhost", "root", "", "wimsfinaldatabase");
 $user_id = $_SESSION['uid'];
 $dataSet = $db->getMeetings("SELECT * FROM `events` WHERE user_id = " . $user_id);
 
@@ -12,13 +12,13 @@ $dataSet = $db->getMeetings("SELECT * FROM `events` WHERE user_id = " . $user_id
 
 
 if ($dataSet) {
-    echo '<table cellpadding="0" cellspacing="0" class="table table-hover">';
-    echo '<tr><th>Τίτλος</th><th>Περιγραφή</th><th>Τοποθεσία</th><th colspan="2" style="text-align:center;" >Επιλογές<th></tr>';
+    echo '<table cellpadding="0" cellspacing="0" class="table table-hover table-responsive">';
+    echo '<tr class="success"><th><span class="glyphicon glyphicon-pushpin"></span>&nbsp;Τίτλος</th><th><span class="glyphicon glyphicon-comment"></span>&nbsp; Περιγραφή</th><th><span class="glyphicon glyphicon-map-marker"></span>&nbsp; Τοποθεσία</th><th colspan="2" style="text-align:center;" ><span class="glyphicon glyphicon-wrench"></span>&nbsp; Επιλογές<th></tr>';
     foreach ($dataSet as $data) {
 
-        echo '<tr>';
+        echo '<tr class="info">';
         echo '<td>', $data->getActiveTitle(), '</td>';
-        echo '<td>', $data->getActivePerigrafi(), '</td>';
+        echo '<td style="width:25%">', $data->getActivePerigrafi(), '</td>';
         echo '<td>', $data->getActiveLocation(), '</td>';
         if ($data->getActiveActive() == 1) {
             echo '<td><button class="btn btn-success" type="button" onClick="getFinal(' . $data->getActiveId() . ');" >Αποτελέσματα Ψηφοφορίας</button>
