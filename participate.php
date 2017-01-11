bcfc4bc8ee5c0b035ee48db74afd7c96edd407ea
Ioannis Christoudis <ioannis.christoudis@gmail.com>
2017-01-11 21:21:29 +0200

    graphic icons plus responsive

--- participate.php
+++ participate.php
@@ -2,7 +2,7 @@
 
 session_start();
 include 'functions.php';
-$db = new Database("localhost", "root", "", "wimsfinal");
+$db = new Database("localhost", "root", "", "wimsfinaldatabase");
 $id_event = $_POST['id_event'];
 $user_id = $_SESSION['uid'];
 
@@ -12,12 +12,12 @@ foreach ($dataSet as $data) {
 }
 
 if ($dataSet) {
-    echo '<table cellpadding="0" cellspacing="0" class="table table-hover">';
-    echo '<tr><th>Συμμετέχοντες</th></tr>';
+    echo '<table cellpadding="0" cellspacing="0" class="table table-hover table-responsive">';
+    echo '<tr class="success"><th><span class="glyphicon glyphicon-user"></span>&nbsp;Συμμετέχοντες</th></tr>';
     for ($i = 0; $i < count($emailParticipants); $i++) {
         $dataSet1 = $db->getUser("SELECT * FROM `user` WHERE email ='" . $emailParticipants[$i] . "' ");
         foreach ($dataSet1 as $data1) {
-            echo '<tr>';
+            echo '<tr class="info">';
             echo '<td class="text-info">', $data1->getUserDisplayName(), '</td>';
             echo '</tr>';
         }
